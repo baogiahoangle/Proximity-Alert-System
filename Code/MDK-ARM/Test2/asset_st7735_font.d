b737678877aea168a4ec7a88fd5e0f1bce223a2e
baogiahoangle <hoanghoanglelegia44@gmail.com>
2025-10-08 16:44:36 +0700

    chore(project): initialize the project

---
+++ Code/MDK-ARM/Test2/asset_st7735_font.d
@@ -0,0 +1,3 @@
+test2\asset_st7735_font.o: ..\DeviceDriver\asset_st7735_font.c
+test2\asset_st7735_font.o: ..\DeviceDriver\asset_st7735_font.h
+test2\asset_st7735_font.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
